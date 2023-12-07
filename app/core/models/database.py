e8f2cf4f1b1977db2b550689644271b42eec6bc5
Nhật Huy <nhathuyd4hp@gmail.com>
2023-12-08 01:58:01 +0700

    database.py

--- app/core/models/database.py
+++ app/core/models/database.py
@@ -1,36 +1,71 @@
 from sqlalchemy import Column, Integer, String, Enum, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+import sqlalchemy
+from sqlalchemy.orm import relationship, DeclarativeBase
 import enum
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+from sqlalchemy.exc import SQLAlchemyError
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
 
 
 class RoleEnum(enum.Enum):
     Manager = "Manager"
     Staff = "Staff"
-    Seller = "Seller"
-    Buyer = "Buyer"
+    Customer = "Customer"
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String)
-    password = Column(String)
-    email = Column(String, unique=True)
-    role = Column(Enum(RoleEnum))
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    username = Column(String(20), unique=True, nullable=False)
+    password = Column(String(256))
+    email = Column(
+        String(50),
+        unique=True,
+    )
+    role = Column(
+        Enum(RoleEnum),
+        default="Customer",
+    )
 
-    items = relationship("Item", back_populates="seller")
+    def json(self):
+        return {
+            "username": self.username,
+            "password": self.password,
+            "email": self.email,
+            "role": self.role,
+        }
 
 
-class Item(Base):
-    __tablename__ = "items"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    description = Column(String, default=None)
-    price = Column(Integer)
-    seller_id = Column(Integer, ForeignKey("users.id"))
+load_dotenv()
+DATABASE_CONFIG = {
+    "USER": os.getenv("MYSQL_USER"),
+    "PASSWORD": os.getenv("MYSQL_PASSWORD"),
+    "HOST": os.getenv("MYSQL_HOST"),
+    "PORT": os.getenv("MYSQL_PORT"),
+    "DATABASE": os.getenv("MYSQL_DATABASE"),
+}
 
-    seller = relationship("User", back_populates="items")
+USER = DATABASE_CONFIG["USER"]
+PASSWORD = DATABASE_CONFIG["PASSWORD"]
+HOST = DATABASE_CONFIG["HOST"]
+PORT = DATABASE_CONFIG["PORT"]
+DATABASE = DATABASE_CONFIG["DATABASE"]
+
+engine = create_engine(f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+
+
+def initialDatabase():
+    try:
+        engine.connect()
+        print("DATABASE is running")
+        lstTable = sqlalchemy.inspect(engine).get_table_names()
+        if len(lstTable) == 0:
+            Base.metadata.create_all(engine)
+    except SQLAlchemyError as error:
+        raise (error)
